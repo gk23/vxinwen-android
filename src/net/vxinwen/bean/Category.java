099913f28048df20610149979f7d297fe6483a15
gk23 <aoaogk@gmail.com>
2012-12-20 18:14:10 +0800

    test commit branch

--- src/net/vxinwen/bean/Category.java
+++ src/net/vxinwen/bean/Category.java
@@ -1,9 +1,27 @@
 package net.vxinwen.bean;
 
+
 public class Category {
 	private long id;
 	private String name;
-
+	private int unreadNewsCount;
+	/**
+	 * 
+	 */
+	private long lastNewsId;
+	
+	public long getLastNewsId() {
+		return lastNewsId;
+	}
+	public void setLastNewsId(long lastNewsId) {
+		this.lastNewsId = lastNewsId;
+	}
+	public int getUnreadNewsCount() {
+		return unreadNewsCount;
+	}
+	public void setUnreadNewsCount(int unreadNewsCount) {
+		this.unreadNewsCount = unreadNewsCount;
+	}
 	public long getId() {
 		return id;
 	}
